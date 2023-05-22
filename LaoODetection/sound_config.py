18e387d958e5aea7c483f8f5465b6bff735a1fc2
Oui Sinthala <sinthala.og.tsg@gmail.com>
2023-05-22 20:57:37 +0700

    Update sound_config.py

--- LaoODetection/sound_config.py
+++ LaoODetection/sound_config.py
@@ -1,7 +1,7 @@
 import subprocess
 def emit_sound():
   # Get the path to the system beep sound file.
-  beep_file = "/Users/mac/Desktop/LaoODetection/LaoODetection/game_sound.mp3"
+  beep_file = "LaoODetection/game_sound.mp3"
 
   # Play the sound file.
   subprocess.Popen(["afplay", beep_file])
