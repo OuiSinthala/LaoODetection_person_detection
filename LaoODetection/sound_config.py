e4868726b435a7360a63fddbdd808c2d928080c1
Oui Sinthala <sinthala.og.tsg@gmail.com>
2023-05-25 13:42:53 +0700

    update

--- LaoODetection/sound_config.py
+++ LaoODetection/sound_config.py
@@ -1,9 +1,14 @@
 import subprocess
+import time
 
 def play_sound():
-    mp3_file_path = '/Users/mac/Desktop/LaoODetection_1_project/LaoODetection/sounds/hello3.mp3'
+    mp3_file_path1 = r'LaoODetection/sounds/hello_ceit.wav'
+    mp3_file_path2 = r'LaoODetection/sounds/hello_CE.wav'
+    mp3_file_path3 = r'LaoODetection/sounds/hell0_CE_president.wav'
+    mp3_file_path4 = r"LaoODetection/sounds/president_iceit.wav"
+
     try:
-        subprocess.run(['afplay', mp3_file_path])
+        subprocess.run(['afplay', mp3_file_path4])
     except FileNotFoundError:
         print("afplay command not found. Make sure you're running macOS.")
     except Exception as e:
